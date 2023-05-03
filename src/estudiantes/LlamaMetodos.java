6b98c432cea06adba4e549438d7e81f1f70da886
juanesteban22 <jestenieto@gmail.com>
2023-05-02 19:22:23 -0500

    Se agrego el Proyecto

---
+++ src/estudiantes/LlamaMetodos.java
@@ -0,0 +1,10 @@
+package estudiantes;
+
+public class LlamaMetodos {
+	public static void main (String [] args) {
+		MetodoEstudiantes mimetodo = new MetodoEstudiantes();
+		
+		mimetodo.iniciar();
+		
+	}
+}
